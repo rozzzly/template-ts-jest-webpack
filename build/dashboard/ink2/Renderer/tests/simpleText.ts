5e452f1e808fe937f21e6ed0779c70685b15acc2
rozzzly <pclienau@gmail.com>
2018-11-25 13:23:09 -0600

    fix source maps

--- build/dashboard/ink2/Renderer/tests/simpleText.ts
+++ build/dashboard/ink2/Renderer/tests/simpleText.ts
@@ -20,6 +20,19 @@ describe('a single (unclipped) text node without embedded Ansi styles', () => {
                 expect(textSpan.x1).toBe(11);
                 expect(textSpan.y).toBe(0);
             });
+            it('plots the RootNode to the rest of the GridRows', () => {
+                expect(grid.rows[0].spans.length).toBe(2);
+                const rootSpan = grid.rows[0].spans[1];
+                expect(rootSpan.node).toBe(grid.root);
+                expect(rootSpan.x0).toBe(11);
+                expect(rootSpan.x1).toBe(20);
+                expect(rootSpan.y).toBe(0);
+                expect(grid.rows[1].spans.length).toBe(1);
+                expect(grid.rows[1].spans[0].x0).toBe(0);
+                expect(grid.rows[1].spans[0].x1).toBe(20);
+                expect(grid.rows[1].spans[0].y).toBe(1);
+
+            });
         });
     });
 });
